846298605f5d7fc664e7fa21fdeb4899e6e771ca
Emilio Salgado <salgado_villagomez_emilio@hotmail.com>
2016-11-13 21:19:34 -0600

    modified android-tipCalc

--- app/src/main/java/com/emilio/android_tipcalc/adapters/TipAdapter.java
+++ app/src/main/java/com/emilio/android_tipcalc/adapters/TipAdapter.java
@@ -1,6 +1,7 @@
 package com.emilio.android_tipcalc.adapters;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +10,12 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.jar.Attributes;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.emilio.android_tipcalc.R;
+import com.emilio.android_tipcalc.db.TipsDatabase;
 import com.emilio.android_tipcalc.entity.TipRecord;
 import com.emilio.android_tipcalc.utils.TipUtils;
 import com.raizlabs.android.dbflow.sql.language.Select;
@@ -65,7 +68,7 @@ public class TipAdapter extends RecyclerView.Adapter<TipAdapter.ViewHolder> {
     public void add(TipRecord record){
         //dataset.add(0, record);
         record.save();
-
+        
         dataset = new Select().from(TipRecord.class).queryList();
 
         notifyDataSetChanged();
